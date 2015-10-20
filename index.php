43d24887c3b09def8a1f7334e34458be645def1c
Faizan Ayubi <indianayubi@gmail.com>
2015-10-20 15:58:08 +0530

    Added Tracking

--- index.php
+++ index.php
@@ -57,6 +57,31 @@ function send($id) {
 
 <body>
 
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      switch(window.location.host){
+        case "bollychitchat.in":
+            ga('create', 'UA-69054592-2', 'auto');
+            break;
+        case "filmyhub.website":
+            ga('create', 'UA-69054592-3', 'auto');
+            break;
+        case "teamfilmy.biz";
+            ga('create', 'UA-69054592-4', 'auto');
+            break;
+        case "teamkapil.website":
+            ga('create', 'UA-69054592-5', 'auto');
+            break;
+      }
+      
+      ga('send', 'pageview');
+
+    </script>
+
     <script type="text/javascript">
         function setCookie(cname, cvalue, exdays) {
             var d = new Date();
